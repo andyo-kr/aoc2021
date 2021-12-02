fc98209e870516d836f4ed87afb20515c601fed2
Andy Orahoske <andy.orahoske@kroger.com>
2021-12-01 20:57:55 -0500

    got b correct

--- src/1/index.ts
+++ src/1/index.ts
@@ -2,17 +2,35 @@ import { readFile } from 'fs'
 import { promisify } from 'util'
 import { resolve } from 'path'
 
-const app = async () => {
-  const result = (await promisify(readFile)(resolve(__dirname, './input/input.txt'))).toString().split('\n')
+const a = async (input: number[]) => {
+  const increase = input.reduce((accum, curr, index) => (curr < input[index + 1] ? accum + 1 : accum), 0)
 
-  const increase = result.reduce((accum, curr, index) => {
-    if (index > result.length - 2) return accum
-    if (parseInt(curr) < parseInt(result[index + 1])) return accum + 1
+  console.log(increase)
+}
+
+const b = async (input: number[]) => {
+  const increase = input.reduce((accum, curr, index) => {
+    if (index > input.length - 4) return accum
+
+    const a = curr + input[index + 1] + input[index + 2]
+    const b = input[index + 1] + input[index + 2] + input[index + 3]
+
+    if (a < b) return accum + 1
 
     return accum
   }, 0)
 
   console.log(increase)
+}
+
+const app = async () => {
+  const initialArray = (await promisify(readFile)(resolve(__dirname, './input/input.txt'))).toString().split('\n')
+  const input = initialArray.map(s => parseInt(s, 10)).filter(n => !Number.isNaN(n))
+
+  console.log('a')
+  await a(input)
+  console.log('b')
+  await b(input)
 }
 
 app()
