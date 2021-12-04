31bdc678a1cc40fc1b9cb454caaafc9eed9ad277
Andy Orahoske <andy.orahoske@kroger.com>
2021-12-03 20:05:15 -0500

    day 3 part b

--- src/3/index.ts
+++ src/3/index.ts
@@ -2,25 +2,28 @@ import { readFile } from 'fs'
 import { resolve } from 'path'
 import { promisify } from 'util'
 
-type Counts = { 0: number; 1: number }
+type Counts = { zero: number; one: number }
 type Calc = {
   gamma: string
   epsilon: string
 }
 
-const a = (input: string[]) => {
-  const defaultCounts: Counts = { 0: 0, 1: 0 }
-  const result = input.reduce<Counts[]>((accum, curr) => {
+const defaultCounts: Counts = { zero: 0, one: 0 }
+
+const makeInputs = (arr: string[]): Counts[] =>
+  arr.reduce<Counts[]>((accum, curr) => {
     const parts = curr.split('')
 
     return parts.map<Counts>((curr, index) => {
       const counts = accum[index] || defaultCounts
-      return { 0: counts['0'] + (curr === '0' ? 1 : 0), 1: counts['1'] + (curr === '1' ? 1 : 0) }
+      return { zero: counts.zero + (curr === '0' ? 1 : 0), one: counts.one + (curr === '1' ? 1 : 0) }
     })
   }, [])
-  const final = result.reduce<Calc>(
+
+const a = (input: Counts[]) => {
+  const final = input.reduce<Calc>(
     (accum, curr) => {
-      if (curr['0'] > curr['1']) {
+      if (curr.zero > curr.one) {
         return {
           gamma: accum.gamma + '0',
           epsilon: accum.epsilon + '1'
@@ -40,12 +43,52 @@ const a = (input: string[]) => {
   console.log(parsedFinal.epsilon * parsedFinal.gamma)
 }
 
+const filterInputsOxygen = (arr: string[], matcher: string = '', length: number = 1): string | undefined => {
+  if (length === arr.length) return undefined
+
+  const newMatch = makeInputs(arr)
+    .map(i => (i.zero <= i.one ? '1' : '0'))
+    .join('')
+  const filteredInputs = arr.filter(v => v.substring(0, length) === `${matcher}${newMatch[length - 1]}`)
+
+  if (filteredInputs.length === 1) return filteredInputs[0]
+
+  return filterInputsOxygen(filteredInputs, `${matcher}${newMatch[length - 1]}`, length + 1)
+}
+
+const filterInputsCO2 = (arr: string[], matcher: string = '', length: number = 1): string | undefined => {
+  if (length === arr.length) return undefined
+
+  const newMatch = makeInputs(arr)
+    .map(i => (i.zero <= i.one ? '0' : '1'))
+    .join('')
+  const filteredInputs = arr.filter(v => v.substring(0, length) === `${matcher}${newMatch[length - 1]}`)
+
+  if (filteredInputs.length === 1) return filteredInputs[0]
+
+  return filterInputsCO2(filteredInputs, `${matcher}${newMatch[length - 1]}`, length + 1)
+}
+
+const b = (initial: string[]) => {
+  const oxygen = filterInputsOxygen(initial)
+  const co2 = filterInputsCO2(initial)
+
+  if (!oxygen || !co2) throw new Error('ughhh')
+
+  console.log(parseInt(oxygen, 2) * parseInt(co2, 2))
+}
+
 const app = async () => {
-  const initialArray = (await promisify(readFile)(resolve(__dirname, './input.txt'))).toString().split('\n')
-  const input = initialArray.filter(i => i !== undefined && i.length > 0)
+  const initialArray = (await promisify(readFile)(resolve(__dirname, './input.txt')))
+    .toString()
+    .split('\n')
+    .filter(i => i !== undefined && i.length > 0)
+  const input = makeInputs(initialArray)
 
   console.log('a')
   a(input)
+  console.log('b')
+  b(initialArray)
 }
 
 app()
